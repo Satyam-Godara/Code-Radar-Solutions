6907b0bea72997c8749952555f718dc7d68fd732
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-01-24 15:30:21 +0530

    Solution to Traffic Light Simulation

---
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main() {
+    char color;
+    scanf("%c",&color);
+    (color=='R')?printf("Stop"):((a=="G")?printf("Go"):((a=="Y")?printf("Slow Down"):printf("Invalid input")));
+    return 0;
+}
