5c9377aa2d950d5e38af4b3f3bcca61c42411b1f
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-02-07 17:38:32 +0530

    Solution to Print Numbers from 1 to N

---
+++ Print Numbers from 1 to N@@676c4885c18c9c17a0f50666/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a,isprime,i,j;
+    scanf("%d",&a);
+    for (i=2;i<=a;i++){
+        isprime=1;
+        for (j=2;j*j<=i;j++){
+            if (i%j==0){isprime=0; break}
+        }
+        if (isprime){printf("%d\n",i);}
+    }
+return 0;
+}
