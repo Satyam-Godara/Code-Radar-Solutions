880d86a447d188483bbf4de2e9ff49b8f1798191
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-02-09 01:23:47 +0530

    Solution to Print an Alphabet Right-Angled Triangle

--- Print an Alphabet Right-Angled Triangle@@67877e1818d53eb724f435c5/code.c
+++ Print an Alphabet Right-Angled Triangle@@67877e1818d53eb724f435c5/code.c
@@ -5,12 +5,12 @@ int main() {
 //    printf("Enter an uppercase character you want to print in the last row: ");
    scanf("%d", &input);
    for (i = 1; i <=input; ++i) {
-    
+    alphabet='A';
       for (j = 1; j <= i; ++j) {
          printf("%c ", alphabet);
       
       ++alphabet;}
-      alphabet='A';
+      
       printf("\n");
    }
    return 0;
