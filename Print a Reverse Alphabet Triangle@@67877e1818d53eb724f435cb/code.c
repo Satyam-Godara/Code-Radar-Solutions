5ff39d01317bd8d019bd15090baf2e50ad68b5c3
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-02-09 13:22:13 +0530

    Solution to Print a Reverse Alphabet Triangle

---
+++ Print a Reverse Alphabet Triangle@@67877e1818d53eb724f435cb/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+   int i, j,input;
+   char alphabet;
+//    printf("Enter an uppercase character you want to print in the last row: ");
+   scanf("%d", &input);
+   for (i = input; i >=1; --i) {
+    alphabet='A';
+      for (j = 1; j <= i; ++j) {
+         printf("%c ", alphabet);
+      
+      ++alphabet;}
+      
+      printf("\n");
+   }
+   return 0;
+}
