d13387b390d7a4b09beb2fc0b38431dbff072c97
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-02-11 10:37:39 +0530

    Solution to Print a Hollow Square Pattern

--- Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
+++ Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
@@ -1,20 +1,33 @@
-#include <stdio.h>
+// #include <stdio.h>
+
+// int main(){
+//     int a,i,j,k;
+
+//     scanf("%d",&a);
+
+//     for (i=0;i<a;i++){printf("*");}
+//     printf("\n");
+    
+//     for (j=0;j<(a-2);j++){
+//         for(k=0;k<(a);k++){
+//         (k==0 || k==(a-1))?printf("*"):printf(" ");
+//         }
+//         printf("\n");
+//     }
+    
+//     for (i=0;i<a;i++){printf("*");}
+//     return 0;
+// }
+
+
+#include<stdio.h>
 
 int main(){
     int a,i,j,k;
-
     scanf("%d",&a);
-
-    for (i=0;i<a;i++){printf("*");}
+    for(i=0;i<=a;i++){printf("*");}
     printf("\n");
-    
-    for (j=0;j<(a-2);j++){
-        for(k=0;k<(a);k++){
-        (k==0 || k==(a-1))?printf("*"):printf(" ");
-        }
-        printf("\n");
-    }
-    
-    for (i=0;i<a;i++){printf("*");}
-    return 0;
+    for(j=0;j<(a-2);j++){for(k=0;k<a;k++){(k==0 || k=(a-1)?printf("*"):printf(" ");)}}
+    printf("\n");
+    for(i=0;i<=a;i++){printf("*");}
 }
