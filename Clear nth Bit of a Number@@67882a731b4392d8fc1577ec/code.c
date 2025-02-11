d9e69e59315278bad31e19daeb7d0314582b3263
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-02-11 09:43:56 +0530

    Solution to Clear nth Bit of a Number

--- Clear nth Bit of a Number@@67882a731b4392d8fc1577ec/code.c
+++ Clear nth Bit of a Number@@67882a731b4392d8fc1577ec/code.c
@@ -1,31 +1,42 @@
-/**
- * C program to clear the nth bit of a number
- */
+// /**
+//  * C program to clear the nth bit of a number
+//  */
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-    int num, n, newNum;
+// int main()
+// {
+//     int num, n, newNum;
 
-    /* Input number from user */
-    // printf("Enter any number: ");
-    scanf("%d %d", &num, &n);
+//     /* Input number from user */
+//     // printf("Enter any number: ");
+//     scanf("%d %d", &num, &n);
 
-    /* Input bit number you want to clear */
-    // printf("Enter nth bit to clear (0-31): ");
-    // scanf("%d", &n);
+//     /* Input bit number you want to clear */
+//     // printf("Enter nth bit to clear (0-31): ");
+//     // scanf("%d", &n);
 
-    /*
-     * Left shifts 1 to n times
-     * Perform complement of above
-     * finally perform bitwise AND with num and result of above
-     */
-    newNum = num & (~(1 << n));
+//     /*
+//      * Left shifts 1 to n times
+//      * Perform complement of above
+//      * finally perform bitwise AND with num and result of above
+//      */
+//     newNum = num & (~(1 << n));
 
-    // printf("Bit cleared successfully.\n\n");
-    // printf("Number before clearing %d bit: %d (in decimal)\n", n, num);
-    printf("%d",newNum);
+//     // printf("Bit cleared successfully.\n\n");
+//     // printf("Number before clearing %d bit: %d (in decimal)\n", n, num);
+//     printf("%d",newNum);
 
+//     return 0;
+// }
+
+
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    new= a & (0<<b);
+    printf("%d",new);
     return 0;
 }
