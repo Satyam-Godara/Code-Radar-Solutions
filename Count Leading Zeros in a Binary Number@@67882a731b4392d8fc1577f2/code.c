053e21d9b05c5f1af9791c539195efe6f81b699a
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-02-11 10:09:15 +0530

    Solution to Count Leading Zeros in a Binary Number

--- Count Leading Zeros in a Binary Number@@67882a731b4392d8fc1577f2/code.c
+++ Count Leading Zeros in a Binary Number@@67882a731b4392d8fc1577f2/code.c
@@ -1,26 +1,39 @@
-/**
- * C program to count leading zeros in a binary number using bitwise operator
- */
+// /**
+//  * C program to count leading zeros in a binary number using bitwise operator
+//  */
 
-#include <stdio.h>
-#include <limits.h> // Used for INT_MAX
+// #include <stdio.h>
+// #include <limits.h> // Used for INT_MAX
 
-int main()
-{
-    int num, count;
+// int main()
+// {
+//     int num, count;
 
-    /* Input number from user */
-    // printf("Enter any number: ");
-    scanf("%d", &num);
+//     /* Input number from user */
+//     // printf("Enter any number: ");
+//     scanf("%d", &num);
 
-    count = 0;
-    while(!(num & (~INT_MAX)))
-    {
-        count++;
-        num <<= 1;
-    }
+//     count = 0;
+//     while(!(num & (~INT_MAX)))
+//     {
+//         count++;
+//         num <<= 1;
+//     }
 
-    printf("%d", count);
+//     printf("%d", count);
 
+//     return 0;
+// }
+
+
+
+#include<stdio.h>
+
+int main(){
+    int a,b,i;
+    scanf("%d",&a);
+    b=0;
+    for(i=0;i<32;i++){if((a<<i)&(1<<31)){b=i; break;}}
+    printf("%d",b);
     return 0;
 }
