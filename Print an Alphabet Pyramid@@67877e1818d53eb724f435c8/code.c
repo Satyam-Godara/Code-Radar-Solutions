735f4dd9eced8148590fde2585968b9744af3d85
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-02-11 11:47:27 +0530

    Solution to Print an Alphabet Pyramid

--- Print an Alphabet Pyramid@@67877e1818d53eb724f435c8/code.c
+++ Print an Alphabet Pyramid@@67877e1818d53eb724f435c8/code.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-int main() {
-   int i, j,input;
-   char alphabet;
-//    printf("Enter an uppercase character you want to print in the last row: ");
-   scanf("%d", &input);
-   for (i = 1; i <=input; ++i) {
-    alphabet='A';
-      for (j = 1; j <= i; ++j) {
-         printf("%c ", alphabet);
-      
-      ++alphabet;}
-      
-      printf("\n");
-   }
-   return 0;
+int main(){
+    int a,i,j;
+    scanf("%d",&a);
+    for(i=1;i<=a;++i){
+        for(j=65;j<(i+65);++j){
+            printf("%c ",j);
+        }
+        printf("\n");
+    }
+    return 0;
 }
