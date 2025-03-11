f9d42b665a004af6022c7374c8f96b0f35a2188d
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-03-11 15:25:17 +0530

    Solution to Kth Missing Positive Number

--- Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
+++ Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
@@ -4,6 +4,7 @@ int findKthMissing(int *arr, int n, int k){
     int count=0,z=1;
     for(int i=0;i<n;i++){
         if(arr[i]!=z){count+=1;}
+        z+=1;
         if(count==k){break;}
     }
     return z;
